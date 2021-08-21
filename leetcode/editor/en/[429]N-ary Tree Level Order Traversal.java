0080b165980fd10159c47cfd16d261e589e84a8c
guozhaoliang <guozhaoliang5566@163.com>
2021-08-21 16:18:22 +0800

    1.增加遍历 2增加移除数字

--- leetcode/editor/en/[429]N-ary Tree Level Order Traversal.java
+++ leetcode/editor/en/[429]N-ary Tree Level Order Traversal.java
@@ -56,7 +56,27 @@ class Node {
 
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
-        
+        List<List<Integer>> result = new ArrayList();
+        if(root == null){
+            return result;
+        }
+        Queue<Node> queue = new LinkedList();
+
+        queue.add(root);
+        while(queue.size()>0){
+            int size = queue.size();
+            List<Integer> level= new ArrayList();
+            for(int i=0;i<size;i++){
+                Node node = queue.poll();
+                level.add(node.val);
+                if(node.children!=null){
+                    queue.addAll(node.children);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ src/[402]Remove K Digits.java
@@ -0,0 +1,78 @@
+//Given string num representing a non-negative integer num, and an integer k, re
+//turn the smallest possible integer after removing k digits from num. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: num = "1432219", k = 3
+//Output: "1219"
+//Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 w
+//hich is the smallest.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: num = "10200", k = 1
+//Output: "200"
+//Explanation: Remove the leading 1 and the number is 200. Note that the output 
+//must not contain leading zeroes.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: num = "10", k = 2
+//Output: "0"
+//Explanation: Remove all the digits from the number and it is left with nothing
+// which is 0.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= k <= num.length <= 105 
+// num consists of only digits. 
+// num does not have any leading zeros except for the zero itself. 
+// 
+// Related Topics String Stack Greedy Monotonic Stack 
+// 👍 3779 👎 160
+
+
+import java.util.Stack;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if(k >= num.length()){
+            return "";
+        }
+
+        Stack<Character> stack = new Stack();
+
+        int i=0;
+        while(i<num.length()){
+            char ch = num.charAt(i);
+            while(k>0 && !stack.isEmpty() && stack.peek()>ch){
+                stack.pop();
+                k = k -1;
+            }
+            stack.push(ch);
+        }
+
+        while(k>0){
+            stack.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+
+        return sb.toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
